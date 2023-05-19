86eef4b3930a7806db0a7159782200604b613f9c
hamiceis <hamiceis@hotmail.com>
2023-05-19 09:15:50 -0300

    removing shadow button

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -7,7 +7,7 @@ interface MenuProps {
 export function Menu({ handlePrint }: MenuProps) {
   return (
     <div
-      className={`shadow-2xl z-50 flex flex-col gap-1 p-1 rounded fixed bottom-0 right-0 -translate-x-1/2 -translate-y-1/2 transition-transform`}
+      className={`z-50 flex flex-col gap-1 p-1 rounded fixed bottom-0 right-0 -translate-x-1/2 -translate-y-1/2 transition-transform`}
     >
       <button
         className="bg-slate-50 flex items-center justify-center w-12 h-12 rounded-full text-2xl border-none cursor-pointer atransition-colors animate-bounce hover:animate-none hover:bg-slate-400 "
